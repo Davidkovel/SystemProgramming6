646c24e2c171a66ff07b305501702e8fd076aa89
David <dkovel7@gmail.com>
2025-04-29 16:57:20 +0100

    Lab task 1 and 6 are done

--- SystemProgramming6/MainWindow.xaml.cs
+++ SystemProgramming6/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SystemProgramming6;
 
@@ -16,8 +10,55 @@ namespace SystemProgramming6;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private Mutex mutex = new Mutex();
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void StartButton_Click(object sender, RoutedEventArgs e)
+    {
+        OutputTextBox.Clear();
+        StartButton.IsEnabled = false;
+        
+        Thread thread1 = new Thread(() =>
+        {
+            mutex.WaitOne();
+
+            for (int i = 0; i <= 20; i++)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    OutputTextBox.AppendText($"Thread 1: {i}\n");
+                    OutputTextBox.ScrollToEnd();
+                });
+                Thread.Sleep(300);
+            }
+
+            mutex.ReleaseMutex();
+        });
+
+        Thread thread2 = new Thread(() =>
+        {
+            mutex.WaitOne();
+
+            for (int i = 10; i >= 0; i--)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    OutputTextBox.AppendText($"Thread 2: {i}\n");
+                    OutputTextBox.ScrollToEnd();
+                });
+                Thread.Sleep(300);
+            }
+
+            mutex.ReleaseMutex();
+
+            Dispatcher.Invoke(() => StartButton.IsEnabled = true);
+        });
+
+        thread1.Start();
+        thread2.Start();
     }
 }
