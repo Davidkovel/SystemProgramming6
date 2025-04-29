c27cf3424518a6f52a8f12ac9a24548c8ccc941b
David <dkovel7@gmail.com>
2025-04-29 17:45:16 +0100

    Lab Task 2,3 and 7 are done

--- SystemProgramming6/MainWindow.xaml.cs
+++ SystemProgramming6/MainWindow.xaml.cs
@@ -16,8 +16,92 @@ namespace SystemProgramming6;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private Mutex arrayMutex = new Mutex();
+    private int[] dataArray = new int[10];
+    private Random random = new Random();
+
     public MainWindow()
     {
         InitializeComponent();
+        GenerateNewArray(); 
+        DisplayArray();
+    }
+
+    private void GenerateNewArray()
+    {
+        for (int i = 0; i < dataArray.Length; i++)
+        {
+            dataArray[i] = random.Next(1, 100);
+        }
+    }
+
+    private void DisplayArray()
+    {
+        OutputTextBox.Text = "Current Array:\n";
+        OutputTextBox.AppendText(string.Join(", ", dataArray) + "\n\n");
+    }
+
+    private void GenerateButton_Click(object sender, RoutedEventArgs e)
+    {
+        GenerateNewArray();
+        DisplayArray();
+        MaxValueText.Text = "";
+    }
+
+    private async void StartButton_Click(object sender, RoutedEventArgs e)
+    {
+        StartButton.IsEnabled = false;
+        GenerateButton.IsEnabled = false;
+        MaxValueText.Text = "";
+        OutputTextBox.AppendText("--- Starting calculating ---\n");
+
+        Thread modifierThread = new Thread(() =>
+        {
+            arrayMutex.WaitOne();
+
+            int increment = random.Next(1, 20);
+            Dispatcher.Invoke(() => OutputTextBox.AppendText($"\nThread 1: Adding new element{increment}\n"));
+
+            for (int i = 0; i < dataArray.Length; i++)
+            {
+                dataArray[i] += increment;
+                Dispatcher.Invoke(() =>
+                {
+                    OutputTextBox.AppendText($"Thread 1: Element {i} = {dataArray[i]}\n");
+                    OutputTextBox.ScrollToEnd();
+                });
+                Thread.Sleep(300);
+            }
+
+            Dispatcher.Invoke(() => OutputTextBox.AppendText("\nThread 1: Array after modification:\n" +
+                                                             string.Join(", ", dataArray) + "\n\n"));
+
+            arrayMutex.ReleaseMutex();
+        });
+
+        Thread maxFinderThread = new Thread(() =>
+        {
+            
+            arrayMutex.WaitOne();
+
+            int max = dataArray.Max();
+            Dispatcher.Invoke(() =>
+            {
+                OutputTextBox.AppendText($"Thread 2: Maximum Array number = {max}\n");
+                MaxValueText.Text = $"Maximum Array number: {max}";
+                OutputTextBox.ScrollToEnd();
+            });
+
+            arrayMutex.ReleaseMutex();
+
+            Dispatcher.Invoke(() =>
+            {
+                StartButton.IsEnabled = true;
+                GenerateButton.IsEnabled = true;
+            });
+        });
+
+        modifierThread.Start();
+        maxFinderThread.Start();
     }
 }
