6c487579239f8e3bfb57dde3536c907708e8d232
David <dkovel7@gmail.com>
2025-04-29 18:01:18 +0100

    Lab task 4 is done

--- SystemProgramming6/MainWindow.xaml.cs
+++ SystemProgramming6/MainWindow.xaml.cs
@@ -16,8 +16,44 @@ namespace SystemProgramming6;
 /// </summary>
 public partial class MainWindow : Window
 {
+    static Mutex mutex = new Mutex(true, "{8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F}");
+
     public MainWindow()
     {
         InitializeComponent();
+
+        if (!mutex.WaitOne(TimeSpan.Zero, true))
+        {
+            MessageBox.Show("\n App is running",
+                "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            Application.Current.Shutdown();
+            return;
+        }
+    }
+
+    private void Start_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            RunApplication();
+        }
+        finally
+        {
+            ;
+        }
+    }
+
+    private void RunApplication()
+    {
+        MessageBox.Show($"App is sucessfuly started (PID: {System.Diagnostics.Process.GetCurrentProcess().Id})\n" +
+                        "This prod can only start with one copy.",
+            "Information about prod: ", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        base.OnClosed(e);
+        mutex.ReleaseMutex();
+        mutex.Dispose();
     }
 }
